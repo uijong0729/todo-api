93ab6e560a0716bc2eb96812588602c550ec354f
uijong0729 <krocha0729@gmail.com>
2023-12-23 21:59:41 +0900

    초기 sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,7 @@
 INSERT INTO samples (content) values ('hello');
+
+INSERT INTO tasks (title) VALUES ('task1');
+INSERT INTO tasks (title) VALUES ('task2');
+INSERT INTO tasks (title) VALUES ('task3');
+INSERT INTO tasks (title) VALUES ('task4');
+INSERT INTO tasks (title) VALUES ('task5');
